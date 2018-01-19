c39b659542f309a48f212d2dd82a015a9ba2f963
a201190 <amitkumarsharma201190@gmail.com>
2018-01-19 20:21:33 +0530

    fifth

--- src/Containers/contact/index.js
+++ src/Containers/contact/index.js
@@ -3,7 +3,31 @@ import './index.scss';
 class Contact extends React.Component{
     render(){
         return(
-            <h1>Contact</h1>
+            <div style={{color:'white', textAlign:'center'}}>
+                <div className="tfv">
+                    <h1>Thanks for visiting!</h1>
+                </div>
+                <div className="cont">
+                    <h5>I would love to hear more from you.</h5>
+                
+                <h5>Want to know more about my projects? My work? Or want to provide me an opportunity of working with you?</h5>
+                <div>
+                    <i className="fa fa-envelope" aria-hidden="true">
+                        amitkumarsharma201190@gmail.com
+                    </i>
+                </div>
+                <div>
+                    <i className="fa fa-linkedin" aria-hidden="true">
+                        https://www.linkedin.com/in/amit-sharma-232636a8/
+                    </i>
+                </div>
+                <div>
+                    <i className="fa fa-angellist" aria-hidden="true">
+                        https://angel.co/amit-kumar-sharma-13?public_profile=1
+                    </i>
+                </div>
+                </div>
+            </div>
         );
     }
 }
--- src/Containers/portfolio/index.js
+++ src/Containers/portfolio/index.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import './index.scss';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button } from 'reactstrap';  
+import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';  
 class Portfolio extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            data:[
+                {
+                    img:require("../../image/port1.png"), header:'Survey', about:`I Developed and Design asurvey web, in which i connect an MongoDb database, when you click on Login with gmail to login your account, after login you click on plus button to open new surve then you fill the form to send your surve to save on Database`, link:'https://asurvey.herokuapp.com'
+                },{
+                    img:require("../../image/port2.png"), header:'Pokemon' ,about:'I Developed and Design apokemon web, in which i connect an MongoDb database, when you click on Login with facebook to login your account, after login you click on search box to search pokemon', link:'https://apokemon.herokuapp.com'                   
+                },{
+                    img:require("../../image/port3.png"), header:'Replica Facebook' ,about:'I Developed and Design areplicafb web, in which i do only frontend', link:'https://areplicafb.herokuapp.com'                 
+                }
+            ]
+        }
+    }
     render(){
         return(
            <div>
-               <Card>
-                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+               <Card className="Portfil">
+                    <CardImg top width="100%" height='300px' src={require('../../image/leopetra.png')} alt="Card image cap" />
                     <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    <Button>Button</Button>
+                    <CardTitle>Leopetra</CardTitle>
+                    <CardText>
+                        . I Developed and Design Leopetra Admin Panel <br/>
+                        . Full​ ​Responsive​ ​Website​,​ ​with​ ​admins​ ​having​ ​ability​ ​to  update​ ​content​ ​on​ ​server<br/>
+                        . Technologies​ ​Used:​ ​​NodeJS,​ ​Express,​ ​jQuery, ReactJs 
+                    </CardText>
                     </CardBody>
                 </Card>
+                <div className="port">
+                {this.state.data.map((data, i)=>{
+                    return(
+                            <Card className="acard" key={i}>
+                            <CardImg top width="100%" height="200px" src={data.img} alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle>{data.header}</CardTitle>
+                                <CardText>{data.about}</CardText>
+                                <a href={data.link}>click here to open web</a>
+                            </CardBody>
+                            </Card>
+                    )
+                })}
+                </div>
            </div>
         );
     }
